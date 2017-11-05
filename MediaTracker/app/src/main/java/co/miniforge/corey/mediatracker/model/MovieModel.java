93719f3986e1b4c26a917ccb59d00e37c5b84b55
h4r3krsna <manju.sreedaran@gmail.com>
2017-11-05 15:02:53 -0800

    Model object renamed

---
+++ MediaTracker/app/src/main/java/co/miniforge/corey/mediatracker/model/MovieModel.java
@@ -0,0 +1,13 @@
+package co.miniforge.corey.mediatracker.model;
+
+import org.json.JSONObject;
+
+/**
+ * Created by corey on 10/20/17.
+ */
+
+public class MovieModel extends MediaItem {
+    public MovieModel(JSONObject jsonObject) {
+        super(jsonObject);
+    }
+}
