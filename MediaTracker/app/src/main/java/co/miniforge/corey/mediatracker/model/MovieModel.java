885697fa066317916c510e2cc8e5a23da31a79ac
h4r3krsna <manju.sreedaran@gmail.com>
2017-11-05 15:11:38 -0800

    MovieModel updated with extra fields
    int myRating, String genre

--- MediaTracker/app/src/main/java/co/miniforge/corey/mediatracker/model/MovieModel.java
+++ MediaTracker/app/src/main/java/co/miniforge/corey/mediatracker/model/MovieModel.java
@@ -1,5 +1,8 @@
 package co.miniforge.corey.mediatracker.model;
 
+import android.util.Log;
+
+import org.json.JSONException;
 import org.json.JSONObject;
 
 /**
@@ -7,7 +10,27 @@ import org.json.JSONObject;
  */
 
 public class MovieModel extends MediaItem {
+    public int myRating;
+    public String genre;
     public MovieModel(JSONObject jsonObject) {
         super(jsonObject);
+        try {
+            this.myRating = jsonObject.getInt("myRating");
+            this.genre = jsonObject.getString("genre");
+        } catch (JSONException e) {
+            Log.e("MOVIECONSTRUCTOR", e.getMessage());
+        }
+    }
+
+    @Override
+    public JSONObject toJson() {
+        JSONObject result = super.toJson();
+        try {
+            result.put("myRating", this.myRating);
+            result.put("genre", this.genre);
+        } catch (JSONException e) {
+            Log.e("MOVIEJSON", e.getMessage());
+        }
+        return result;
     }
 }
