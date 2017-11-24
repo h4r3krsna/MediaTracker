8db5f4e6c73a88c62c0d672c9aff8a5e50e6cbce
h4r3krsna <manju.sreedaran@gmail.com>
2017-11-24 14:21:11 -0800

    RecyclerView *should* show typed itemviews

--- MediaTracker/app/src/main/java/co/miniforge/corey/mediatracker/media_recycler/MediaRecyclerAdapter.java
+++ MediaTracker/app/src/main/java/co/miniforge/corey/mediatracker/media_recycler/MediaRecyclerAdapter.java
@@ -1,6 +1,8 @@
 package co.miniforge.corey.mediatracker.media_recycler;
 
+import android.provider.MediaStore;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,8 +21,45 @@ public class MediaRecyclerAdapter extends RecyclerView.Adapter {
     private List<MediaItem> mediaItems = new LinkedList<>();
 
     @Override
+    public int getItemViewType(int position) {
+        MediaItem mediaItem = mediaItems.get(position);
+        int mediaItemType = -1;
+        switch (mediaItem.type) {
+            case TV:
+                mediaItemType = 1;
+                break;
+            case Movie:
+                mediaItemType = 2;
+                break;
+            case YouTube:
+                mediaItemType = 3;
+                break;
+            case Generic:
+            default:
+                mediaItemType = 0;
+        }
+        return mediaItemType;
+    }
+
+    @Override
     public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View inflated = LayoutInflater.from(parent.getContext()).inflate(R.layout.media_item, parent, false);
+        int layoutType = -1;
+        switch (viewType) {
+            case 1:
+                layoutType = R.layout.tv_item;
+                break;
+            case 2:
+                layoutType = R.layout.movie_item;
+                break;
+            case 3:
+                layoutType = R.layout.youtube_item;
+                break;
+            case 0:
+            default:
+                layoutType = R.layout.media_item;
+                break;
+        }
+        View inflated = LayoutInflater.from(parent.getContext()).inflate(layoutType, parent, false);
         return new MediaViewHolder(inflated);
     }
 
